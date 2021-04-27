5d888c28d889585419dca810db4d5c3268d44dcb
Samuel Adekunle <me@samuel-adekunle.software>
2021-04-27 08:43:23 +0100

    new insight for hamming distance

--- 461 - Hamming Distance/main.go
+++ 461 - Hamming Distance/main.go
@@ -21,17 +21,59 @@ The above arrows point to positions where the corresponding bits are different. 
 
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 // Using conversion to calculate hamming distance
+// func hammingDistance(x int, y int) int {
+// 	dist := 0
+// 	for x > 0 || y > 0 {
+// 		if x%2 != y%2 {
+// 			dist++
+// 		}
+// 		x /= 2
+// 		y /= 2
+// 	}
+// 	return dist
+// }
+
 func hammingDistance(x int, y int) int {
-	dist := 0
-	for x > 0 || y > 0 {
-		if x%2 != y%2 {
-			dist++
+	return hammingWeight(booleanSum(x, y))
+}
+
+func booleanSum(x int, y int) int {
+	bx, by := strconv.FormatInt(int64(x), 2), strconv.FormatInt(int64(y), 2)
+	bx, by = applyEqualPadding(bx, by)
+	bsum := ""
+	for i := 0; i < len(bx); i++ {
+		if bx[i] == by[i] {
+			bsum = bsum + "0"
+		} else {
+			bsum = bsum + "1"
 		}
-		x /= 2
-		y /= 2
 	}
-	return dist
+	sum, _ := strconv.ParseInt(bsum, 2, 64)
+	return int(sum)
+}
+
+func applyEqualPadding(x, y string) (string, string) {
+	for len(x) > len(y) {
+		y = "0" + y
+	}
+	for len(y) > len(x) {
+		x = "0" + x
+	}
+	return x, y
+}
+
+func hammingWeight(x int) (i int) {
+	for x > 0 {
+		i += x & 1
+		x = x >> 1
+	}
+	return
 }
 
 func oneBitCount(x int) int {
@@ -46,4 +88,8 @@ func oneBitCount(x int) int {
 func xorDistance(x int, y int) int {
 	dist := x ^ y
 	return oneBitCount(dist)
+}
+
+func main() {
+	fmt.Println(hammingDistance(3, 1))
 }
