08d9c9d8ae2dc46bd6df9ed42669128931757482
Samuel Adekunle <me@samuel-adekunle.software>
2021-08-17 19:06:59 +0100

    new leetcode structure

---
+++ Old/39 - Combination Sum/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	candidates, target := []int{2,7,6,3,5,1}, 9
+	validCombinations := combinationSum(candidates, target)
+	fmt.Println(validCombinations)
+}
+
+// candidates contains distinct integers
+// assumes candidates is sorted
+func combinationSum(candidates []int, target int) [][]int {
+	sort.Slice(candidates, func(i, j int) bool {
+		return candidates[i] < candidates[j]
+	})
+
+	validCombinations := [][]int{}
+	keys := map[string]bool{}
+	var appendCombination func([]int)
+	appendCombination = func (combination []int)  {
+		sort.Slice(combination, func(i, j int) bool {
+			return combination[i] < combination[j]
+		})
+		key := ""
+		for _,v := range combination {
+			key += fmt.Sprintf("%v", v)
+		}
+		_, ok := keys[key]
+		if !ok {
+			fmt.Println(key)
+			keys[key] = true
+			validCombinations = append(validCombinations, combination)
+		}
+	}
+
+	cache := map[[2]int][]int{} // key {2,5} means starts with 2 and sums to 5
+	var helper func([]int, int)
+	helper = func(curr []int, sum int) {
+		if sum == target {
+			appendCombination(curr)	
+		} else {
+			for _,candidate := range candidates {
+				cache[[2]int{curr[0], sum}] =  curr
+				v, ok := cache[[2]int{candidate, target - sum}]
+				if ok {
+					appendCombination(append(curr, v...))
+				} else if candidate >= curr[0] && (sum + candidate <= target) {
+					helper(append(curr, candidate), sum + candidate)
+				}
+			}
+		}
+	}
+	for _,candidate := range candidates {
+		helper([]int{candidate}, candidate)
+	}
+	return validCombinations
+}
